3a8c92cfd55657dd66f1cb8c0af76bd4d68bb38e
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-18 23:34:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade] !=NIL
+      @roster[grade] << name
+    else
+      @roster[grade]
+      
+      
